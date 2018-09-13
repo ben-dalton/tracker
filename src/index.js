7617f48454547c09210e04139f2d77d99662f55a
ben-dalton <benadalton@gmail.com>
2018-09-13 12:08:11 -0400

    wip

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// const store = createStore(trackerStore);
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root'),
 );
 registerServiceWorker();
